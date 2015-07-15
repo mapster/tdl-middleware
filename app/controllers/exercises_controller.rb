3843ca2708a7d0bf3888edc1e7282cbc04a93388
mapster <alexander@rosbach.no>
2015-07-15 19:15:59 +0200

    chaned exercises controller to get exercise in before_filter

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -2,43 +2,44 @@ class ExercisesController < ResourceBaseController
     @@modifiable = ["name", "title", "kind", "difficulty", "description"]
     @@required = @@modifiable
 
+    before_filter :get_exercise, only: [:show, :update, :destroy]
+
     def index
         render json: Exercise.all
     end
 
     def show
-        render json: (Exercise.find params[:id])
-    rescue ActiveRecord::RecordNotFound
-        render nothing: true, status: :not_found
+        render json: @exercise
     end
 
     def create
-        exercise = Exercise.new(@json)
+        @exercise = Exercise.new(@json)
 
-        if exercise.valid? and exercise.save
-            redirect_to exercise
+        if @exercise.valid? and @exercise.save
+            redirect_to @exercise
         else
-            render json: exercise.errors.messages, status: :conflict
+            render json: @exercise.errors.messages, status: :conflict
         end
     end
 
     def update
-        exercise = Exercise.find(params[:id])
-        if exercise.update(@json)
-            redirect_to exercise
+        if @exercise.update(@json)
+            redirect_to @exercise
         else
-            render json: exercise.errors.messages, status: :bad_request
+            render json: @exercise.errors.messages, status: :bad_request
         end
     end
 
     def destroy
-        exercise = Exercise.find(params[:id])
-        if exercise.destroy
+        if @exercise.destroy
             render nothing: true, status: :no_content
         else
             render nothing: true, status: :internal_server_error
         end
-    rescue ActiveRecord::RecordNotFound
-        render nothing: true, status: :not_found
+    end
+
+    private
+    def get_exercise
+        @exercise = Exercise.find(params[:id])
     end
 end
