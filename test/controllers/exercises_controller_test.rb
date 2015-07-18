57b33958790c858299af9f8bbe56a88e7a08b638
mapster <alexander@rosbach.no>
2015-07-19 01:38:26 +0200

    Generalized auth tests.

--- test/controllers/exercises_controller_test.rb
+++ test/controllers/exercises_controller_test.rb
@@ -2,6 +2,7 @@ require 'test_helper'
 
 class ExercisesControllerTest < ActionController::TestCase
     setup :activate_authlogic
+    include AuthorizationTest
 
     test "should get index" do
         get :index
@@ -9,89 +10,34 @@ class ExercisesControllerTest < ActionController::TestCase
     end
 
     test "should get exercise" do
-        get :show, {'id' => ex}
+        get :show, path_params(fixture)
         assert_response :success
-    end
-
-    # delete
-
-    test "normal user should not be allowed to delete" do
-        UserSession.create users(:jolly)
-        delete :destroy, {'id' => ex}
-        assert_response :forbidden
-    end
-
-    test "authorized user should be allowed to delete" do
-        UserSession.create users(:exercise_manager)
-        delete :destroy, {'id' => ex}
-        assert_response :success
-    end
-
-    test "unauthenticated request should not be allowed to delete" do
-        delete :destroy, {'id' => ex}
-        assert_response :forbidden
-    end
-
-    ## update
-
-    test "normal user should not be allowed to update" do
-        UserSession.create users(:jolly)
-        update ex
-        assert_response :forbidden
-    end
-
-    test "authorized user should be allowed to update" do
-        UserSession.create users(:exercise_manager)
-        update ex
-        assert_response :success
-    end
-
-    test "unauthenticated request should not be allowed to update" do
-        update ex
-        assert_response :forbidden
-    end
-
-    ## create 
-
-    test "create should return created with location in header" do
-        UserSession.create users(:exercise_manager)
-        ex.name = "something else"
-        create ex
-        assert_response :created
-        assert_equal exercise_path(JSON.parse(response.body)['id']), response.location
-    end
-
-    test "normal user should not be allowed to create" do
-        UserSession.create users(:jolly)
-        ex.name = "something else"
-        create ex
-        assert_response :forbidden
-    end
-
-    test "authorized user should be allowed to create" do
-        UserSession.create users(:exercise_manager)
-        ex.name = "something else"
-        create ex
-        assert_response :success
-    end
-
-    test "unauthenticated request should not be allowed to create" do
-        ex.name = "something else"
-        create ex
-        assert_response :forbidden
     end
 
     private
     def create (exercise)
+        fixture.name = "something else"
         post_json :create, select_fields(exercise, ExercisesController::MODIFIABLE)
     end
 
     def update (exercise)
-        put_json :update, {'id' => exercise}, select_fields(exercise, ExercisesController::MODIFIABLE)
+        put_json :update, path_params(exercise), select_fields(exercise, ExercisesController::MODIFIABLE)
     end
 
-    def ex
+    def manager
+        users(:exercise_manager)
+    end
+
+    def fixture
         exercises(:ex1)
+    end
+
+    def path_params exercise
+        {'id' => exercise.id}
+    end
+    
+    def path exercise
+        exercise_path(JSON.parse(response.body)['id'])
     end
 
 end
---
+++ test/shared/autorization_test.rb
@@ -0,0 +1,71 @@
+module AuthorizationTest
+    extend ActiveSupport::Concern
+
+    included do
+        ## delete
+
+        test "normal user should not be allowed to delete" do
+            UserSession.create users(:jolly)
+            delete :destroy, path_params(fixture) 
+            assert_response :forbidden
+        end 
+
+        test "authorized user should be allowed to delete" do
+            UserSession.create manager
+            delete :destroy, path_params(fixture)
+            assert_response :success
+        end 
+
+        test "unauthenticated request should not be allowed to delete" do
+            delete :destroy, path_params(fixture)
+            assert_response :forbidden
+        end 
+
+
+        ## update
+
+        test "normal user should not be allowed to update" do
+            UserSession.create users(:jolly)
+            update fixture
+            assert_response :forbidden
+        end
+
+        test "authorized user should be allowed to update" do
+            UserSession.create manager
+            update fixture
+            assert_response :success
+        end
+
+        test "unauthenticated request should not be allowed to update" do
+            update fixture
+            assert_response :forbidden
+        end
+
+        ## create 
+
+        test "create should return created with location in header" do
+            UserSession.create manager 
+            create fixture
+            assert_response :created
+            assert_equal path(fixture), response.location
+        end
+
+        test "normal user should not be allowed to create" do
+            UserSession.create users(:jolly)
+            create fixture
+            assert_response :forbidden
+        end
+
+        test "authorized user should be allowed to create" do
+            UserSession.create manager
+            create fixture
+            assert_response :success
+        end
+
+        test "unauthenticated request should not be allowed to create" do
+            create fixture
+            assert_response :forbidden
+        end
+
+    end
+end
