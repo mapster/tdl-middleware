9c4061e0e078bac640b0917669ef64140c4dd9cf
mapster <alexander@rosbach.no>
2015-08-08 23:29:48 +0200

    Add seed admin and fix user_session rendering

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -3,8 +3,9 @@ class UserSessionsController < ResourceBaseController
     REQUIRED = MODIFIABLE
 
     def show 
-        if current_user
-            render json: current_user
+        @user = current_user
+        if @user
+            render "users/show"
         else
             render nothing: true, status: :not_found
         end
@@ -13,7 +14,8 @@ class UserSessionsController < ResourceBaseController
     def create
         @user_session = UserSession.new @json
         if @user_session.save
-            render json: current_user
+            @user = current_user
+            render "users/show"
         else
             render nothing: true, status: :not_found
         end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+admin = User.create({name: 'Alexander Hoem Rosbach', email: 'alexander@rosbach.no', password: '123123', password_confirmation: '123123'})
+admin_auth = UserAuthorization.create({user_id: admin.id, manage_exercises: true, manage_users: true, manage_authorizations: true})
