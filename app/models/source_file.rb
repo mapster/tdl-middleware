b91d0116b870ae30176401c1a0a7abd49c4aa2c5
mapster <alexander@rosbach.no>
2015-10-07 22:20:15 +0200

    Add helpers for exercise as SourceFile source_set

--- app/models/source_file.rb
+++ app/models/source_file.rb
@@ -2,4 +2,23 @@ class SourceFile < ActiveRecord::Base
   belongs_to :source_set, :polymorphic => true
   validates :name, presence: true
   validates :source_set, presence: true
+  
+  def initialize(args)
+      if args["exercise_id"]
+          args["source_set_id"] = args["exercise_id"]
+          args["source_set_type"] = "Exercise"
+          args.delete "exercise_id"
+      end
+      super args
+  end
+  
+  def exercise_id
+      return nil unless source_set_type == "Exercise"
+      source_set_id
+  end
+  
+  def exercise
+      return nil unless source_set_type == "Exercise"
+      source_set
+  end
 end
