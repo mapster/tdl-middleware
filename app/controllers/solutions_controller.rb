1f2ff71d5144827b76cc808a0602d75fe7ac32f1
mapster <alexander@rosbach.no>
2015-10-08 21:40:04 +0200

    Add index and show actions to solutions controller

--- app/controllers/solutions_controller.rb
+++ app/controllers/solutions_controller.rb
@@ -1,2 +1,30 @@
 class SolutionsController < ResourceBaseController
+  before_filter :get_exercise, only: [:show]
+  before_filter :get_solution, only: [:show]
+    
+  def index
+    @solutions = Solution.where(user_id: current_user.id)
+  end
+    
+  def show
+    if @solution.nil?
+      @solution = Solution.new :user => current_user, :exercise_id => @exercise.id
+      if not @solution.save
+        render nothing: true, status: :internal_server_error
+      end
+    end
+  end
+    
+  private
+    
+  def get_solution
+   @solution = Solution.find_by(user_id: current_user.id, exercise_id: params[:id])
+  end
+    
+  def get_exercise
+    @exercise = Exercise.find_by_id(params[:id])
+    if @exercise.nil?
+      render nothing: true, status: :not_found and return
+    end
+  end
 end
--- test/controllers/solutions_controller_test.rb
+++ test/controllers/solutions_controller_test.rb
@@ -1,7 +1,47 @@
 require 'test_helper'
 
 class SolutionsControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup :activate_authlogic
+  
+  def setup
+    @user = users :jolly
+    @ex_missing_solution = exercises(:ex_missing_solution)
+    UserSession.create @user
+  end
+
+  test ":show should create solution for exercise if it does not exist" do
+    get_json :show, {:id => @ex_missing_solution}
+    assert_equal @ex_missing_solution.id, assigns(:solution).exercise_id
+  end  
+  
+  test ":show should create a solution with the current user as owner" do
+    get_json :show, {:id => @ex_missing_solution}
+    assert_equal @user.id, assigns(:solution).user_id 
+  end
+  
+  test ":show should return not found for non existing exercise_id" do
+    get_json :show, {:id => 1}
+    assert_response :not_found
+  end
+  
+  test ":show should return existing solution" do
+    get_json :show, {:id => exercises(:ex1)}
+    assert_equal solutions(:jolly_ex1_solution).id, assigns(:solution).id 
+  end
+  
+  test ":index should be successful" do
+    get_json :index
+    assert_response :success
+  end
+  
+  test ":index should return all user solutions" do
+    get_json :index
+    assert_includes assigns(:solutions), solutions(:jolly_ex1_solution)
+    assert_includes assigns(:solutions), solutions(:jolly_ex2_solution)
+  end
+  
+  test ":index should not return other user's soultions" do
+    get_json :index
+    assert_not_includes assigns(:solutions), solutions(:happy_ex1_solution)
+  end
 end
