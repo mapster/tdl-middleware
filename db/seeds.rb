2c5e52cde5966f8f32e01f00bf774aa7bee96d88
Alexander Hoem Rosbach <alexander@rosbach.no>
2017-08-03 21:34:22 +0200

    Add exercise and source files to development db seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,3 +8,15 @@
 
 admin = User.create({name: 'Alexander Hoem Rosbach', email: 'alexander@rosbach.no', password: '123123', password_confirmation: '123123'})
 admin_auth = UserAuthorization.create({user_id: admin.id, manage_exercises: true, manage_users: true, manage_authorizations: true})
+
+if Rails.env == "development"
+  exercise1 = Exercise.create({
+                                  name: 'exercise1',
+                                  title: 'Exercise1',
+                                  kind: 'expectation',
+                                  difficulty: 3,
+                                  description: 'Bla bla',
+                              })
+  SourceFile.create({name: 'File.java', contents: 'noe tekst', source_set: exercise1})
+  SourceFile.create({name: 'File2.java', contents: 'annen tekst', source_set: exercise1})
+end
